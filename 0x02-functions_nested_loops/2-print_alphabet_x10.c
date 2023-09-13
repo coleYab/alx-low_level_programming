48938df276888d846d3c389ea9afe08e45af4a0b
Yeabsira Moges <yabume123@gmail.com>
2023-09-13 04:05:38 -0700

    Print alphabet-10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 	int i;
 	int alphabet;
 
-	for (int i = 1; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (alphabet = 97; alphabet <= 122; alphabet++)
 			_putchar(alphabet);
