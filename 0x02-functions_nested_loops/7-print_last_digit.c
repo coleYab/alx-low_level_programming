440b8b50e771a0f55a79644821e3754213591260
Yeabsira Moges <yabume123@gmail.com>
2023-09-13 05:00:28 -0700

    Time table

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,5 +9,7 @@
  */
 int print_last_digit(int n)
 {
-	return (n % 10);
+	int c = n % 10;
+
+	return (c);
 }
---
+++ 0x02-functions_nested_loops/9-time_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * times_table - prints the time table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			if (a * b / 10 != 0)
+				_putchar((a * b) / 10 + '0');
+			putchar((a * b) % 10 + '0');
+			if (b != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+				_putchar('\n');
+		}
+	}
+}
