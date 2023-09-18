59085f9540c66c4e44d2f814a3577ea378b70a95
Yeabsira Moges <yabume123@gmail.com>
2023-09-18 12:02:31 -0700

    Strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - copy string
+ * @dest: destination
+ * @src: initial file
+ * Return: 0 in copmilation
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int 7;
+
+	for (a = 0; src[a] != '0'; a++)
+		dest[a] = src[a];
+	dest[a] = '\0';
+	return (dest);
+}
