c9836dfb6193ca75a24737bc28dc685cf8d1f89a
Yeabsira Moges <yabume123@gmail.com>
2023-09-17 23:45:23 -0700

    Task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - printd the half of the input
+ * @str: the parameter passed
+ *
+ * Return: void
+ */
+void puts2(char *str);
+{
+	int length = 0;
+
+	while (*str)
+	{
+		if (length++ % 2 == 0)
+			_putchar(*(str++));
+	}
+	_putchar('\n');
+}
