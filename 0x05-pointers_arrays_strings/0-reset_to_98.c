3658b239f047d6c022d9212ad382014588af2bf5
Yeabsira Moges <yabume123@gmail.com>
2023-09-17 22:29:51 -0700

    Task 0 and main.h added

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this is the varibale that reset any input from user
+ * @n: the parameter
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
