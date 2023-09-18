579b81a5395b1d28cecf8023f8497b5d41a4a0fa
Yeabsira Moges <yabume123@gmail.com>
2023-09-18 15:44:29 -0700

    prolly last work

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,39 +1,45 @@
 #include "main.h"
 
 /**
- * _atoi - Searchs intiger from string.
+ * _atoi - Converts a string to an integer.
  * @s: The input string.
  *
  * Return: The converted integer.
  */
 int _atoi(char *s)
 {
-	int myValue = 0;
-	int is_found = 0;
-	int count = 0;
-	int isMinus = 1;
+	int result = 0;
+	int sign = 1;
+	int digit_found = 0;
 
+	while (*s == ' ')
+	{
+		s++;
+	}
+	if (*s == '-' || *s == '+')
+	{
+		if (*s == '-')
+		{
+			sign = -1;
+		}
+		s++;
+	}
 	while (*s)
 	{
-		if (*s == '-' && (*(s + 1) >= '0' && *(s + 1) <= '9'))
-			isMinus = -1;
-
 		if (*s >= '0' && *s <= '9')
 		{
-			if (count == 0)
-				myValue = (int)(*s - '0');
-			else
-				myValue = myValue * 10 + (*s - '0');
-
-			is_found = 1;
+			result = result * 10 + (*s - '0');
+			digit_found = 1;
 		}
-
-		if ((*s < '0' || *s > '9') && is_found == 1)
-			break;
-
+		else
+		{
+			if (digit_found)
+			{
+				break;
+			}
+		}
 		s++;
-		count++;
 	}
 
-	return (isMinus * myValue);
+	return (sign * result);
 }
