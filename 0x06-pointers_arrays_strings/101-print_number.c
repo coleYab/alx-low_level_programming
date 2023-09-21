5965e78bf75aedb5016a9da1491cdf01193b9ee5
Yeabsira Moges <yabume123@gmail.com>
2023-09-21 05:59:11 -0700

    Advanced task

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * print_number - print any integer
+  * @n: parameter one
+  *
+  * Return: void
+  */
+void print_number(int n)
+{
+	unsigned int number;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	number = n;
+	if (number / 10)
+		print_number(number / 10);
+	_putchar(x % 10 + '0');
+}
