69e5f350f3079b4a91a7b7a76b705f7877313e9f
Yeabsira Moges <yabume123@gmail.com>
2023-10-11 05:31:23 -0700

    LEts do it

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,7 +11,6 @@
 int main(int argc, char *argv[])
 {
 	int inta, intb;
-	int (*f)(int, int);
 
 	if (argc != 4)
 	{
@@ -19,15 +18,13 @@ int main(int argc, char *argv[])
 		return (98);
 	}
 
-	f = get_op_func(argv[2]);
-
-	if (f == NULL)
+	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
 		return (99);
 	}
 
-	if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '/' && 
+	if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '/' &&
 		argv[2][0] != '*' && argv[2][0] != '%') || strlen(argv[2]) != 1)
 	{
 		printf("Error\n");
@@ -37,7 +34,7 @@ int main(int argc, char *argv[])
 		inta = atoi(argv[1]);
 		intb = atoi(argv[3]);
 
-		printf("%d\n", f(inta, intb));
+		printf("%d\n", (*get_op_func(argv[2]))(inta, intb));
 
 		return (0);
 }
